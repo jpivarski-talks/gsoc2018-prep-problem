ee7a9b9d254ced46970526dc627da42d080cb89e
Jim Pivarski <jpivarski@gmail.com>
2018-02-26 16:50:11 -0600

    it's working

--- vectorized.py
+++ vectorized.py
@@ -7,7 +7,8 @@ import meta
 
 def fcn(index):
     print "ONE", index
-    print "TWO", index
+    if index % 2 == 0:
+        print "TWO", index
     print "THREE", index
 
 def callexec(code, name):
@@ -24,32 +25,115 @@ def instrument(fcn):
         return out
 
     text = []
-    def recurse(node):
+    def statement(node):
         if isinstance(node, list):
             out = []
             for x in node:
+                t = ("\n" + meta.dump_python_source(x).strip()).replace("\n", "\n    ")
                 out.append(makeyield(len(text)))
-                out.append(x)
-                text.append(meta.dump_python_source(x).strip())
-            out.append(makeyield(len(text)))
+                text.append(t)
+                out.append(statement(x))
             node = out
 
         elif isinstance(node, ast.Module):
-            node.body = recurse(node.body)
+            node.body = statement(node.body)
+
+        elif isinstance(node, (ast.ClassDef, ast.Exec, ast.FunctionDef, ast.Import, ast.Return)):
+            raise SyntaxError("{0} statements are not allowed".format(node.__class__.__text__))
+
+        elif isinstance(node, ast.Expr):
+            node.value = expression(node.value)
+
+        elif isinstance(node, ast.Assert):
+            node.test = expression(node.test)
+
+        elif isinstance(node, ast.Assign):
+            node.targets = [expression(x) for x in node.targets]
+            node.value = expression(node.value)
+
+        elif isinstance(node, ast.AugAssign):
+            node.target = expression(node.target)
+            node.value = expression(node.value)
+
+        elif isinstance(node, ast.Delete):
+            node.targets = [expression(x) for x in node.targets]
+
+        elif isinstance(node, ast.ExceptHandler):
+            node.body = statement(node.body)
+        
+        elif isinstance(node, ast.For):
+            node.iter = expression(node.iter)
+            node.body = statement(node.body)
+            node.orelse = statement(node.orelse)
+
+        elif isinstance(node, ast.If):
+            node.test = expression(node.test)
+            node.body = statement(node.body)
+            node.orelse = statement(node.orelse)
+
+        elif isinstance(node, ast.Print):
+            node.values = expression(node.values)
+
+        elif isinstance(node, ast.TryExcept):
+            node.body = statement(node.body)
+            node.orelse = statement(node.orelse)
+
+        elif isinstance(node, ast.TryFinally):
+            node.body = statement(node.body)
+            node.finalbody = statement(node.finalbody)
+
+        elif isinstance(node, ast.Try):
+            node.body = statement(node.body)
+            node.orelse = statement(node.orelse)
+            node.finalbody = statement(node.finalbody)
+
+        elif isinstance(node, ast.While):
+            node.test = expression(node.test)
+            node.body = statement(node.body)
+            node.orelse = statement(node.orelse)
+
+        elif isinstance(node, ast.With):
+            node.body = statement(node.body)
+            if hasattr(node, "items"):
+                for x in node.items:
+                    x.context_expr = expression(x.context_expr)
+            else:
+                node.context_expr = expression(node.context_expr)
+
+        return node
+
+    def expression(node):
+        if isinstance(node, list):
+            node = [expression(x) for x in node]
+
+        elif isinstance(node, (ast.DictComp, ast.GeneratorExp, ast.IfExp, ast.Lambda, ast.ListComp, ast.SetComp, ast.Yield)):
+            raise SyntaxError("{0} expressions are not allowed".format(node.__class__.__text__))
+
+        elif isinstance(node, ast.AST):
+            for n in node._fields:
+                setattr(node, n, expression(getattr(node, n)))
 
         return node
 
     code = fcn.__code__
     shellfcn = ast.parse("def fcn({0}): pass".format(", ".join(code.co_varnames[:code.co_argcount]))).body[0]
-    shellfcn.body = recurse(meta.decompile(code)).body
+    shellfcn.body = statement(meta.decompile(code)).body
+    shellfcn.body.append(makeyield(len(text)))
+
     instrumented = callexec(compile(ast.Module([shellfcn]), "<vectorized>", "exec"), "fcn").__code__
 
     return len(text), text, types.FunctionType(instrumented, fcn.__globals__, fcn.__name__, fcn.__defaults__, fcn.__closure__)
 
 def vectorize(fcn, numitems, *args, **kwds):
     numsteps, text, instfcn = instrument(fcn)
+    def donext(x):
+        try:
+            return next(x)
+        except StopIteration:
+            return numsteps
+
     iters = [instfcn(i, *args, **kwds) for i in range(numitems)]
-    steps = [next(x) for x in iters]
+    steps = [donext(x) for x in iters]
     while True:
         leading = max(steps)
         atleading = sum(1 if x == leading else 0 for x in steps)
@@ -61,11 +145,11 @@ def vectorize(fcn, numitems, *args, **kwds):
         while atleading < len(steps):
             for i in range(numitems):
                 if steps[i] < leading:
-                    steps[i] = next(iters[i])
+                    steps[i] = donext(iters[i])
             atleading = sum(1 if x == leading else 0 for x in steps)
 
         for i in range(numitems):
             if steps[i] == leading:
-                steps[i] = next(iters[i])
+                steps[i] = donext(iters[i])
 
 vectorize(fcn, 10)
